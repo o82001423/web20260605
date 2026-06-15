b21f114acc6a66515e145d097d8bdd2abb64fda2
d1611234 <d1611234@gmail.com>
2026-06-15 16:13:03 +0800

    新增選單功能

--- include/news.php
+++ include/news.php
@@ -1,13 +1,11 @@
-<h3 class="cent">更換動畫圖片</h3>
+<h3 class="cent">新增最新消息資料</h3>
 <hr>
 <form action="api/add.php?table=news" method="post" enctype="multipart/form-data">
     <table class="all" style="width:70%; margin:auto;">
         <tr>
             <td class="tt">最新消息資料：</td>
             <td>
-                <textarea name="text" style="width:300px;height:200px;">
-
-                </textarea>
+                <textarea name="text" style="width:300px;height:200px;"></textarea>
             </td>
         </tr>
         
--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <div id="cover" style="display:none; ">
+    <div id="cover" style="display:none;">
         <div id="coverr">
             <a style="position:absolute;right:3px;top:4px;cursor:pointer;z-index:9999;" onclick="cl('#cover')">X</a>
             <div id="cvr" style="position:absolute;width:99%;height:100%;margin:auto;z-index:9898;">
@@ -38,13 +38,26 @@
                     $mains=$Menu->all(['sh'=>1,'main_id'=>0]);
                     foreach($mains as $main):?>
 
-                    <a href="<?= $main['href'] ?>">
-                        <div class="mainmu cent"><?= $main['text'] ?></div>
-                    </a>
-                   <?php endforeach;?>
+                    <div class='mainmu cent'>
+                        <a href="<?= $main['href'] ?>"><?= $main['text'] ?></a>
+                        <div class="mw">
+                            <?php 
+                        if($Menu->count(['main_id'=>$main['id']])>0){
+                            $subs=$Menu->all(['main_id'=>$main['id']]);
+                            foreach($subs as $sub):
+                       
+                        ?>
+                            <div class='mainmu2'>
+                                <a href="<?= $sub['href'] ?>"><?= $sub['text'] ?></a>
+                            </div>
+                            <?php endforeach;} ?>
+                        </div>
+                    </div>
+
+                    <?php endforeach;?>
                 </div>
                 <div class="dbor" style="margin:3px;width:95%; height:20%;line-height:100px;">
-                    <span class="t"><?= $Total->find(1)['total'] ?>  </span>
+                    <span class="t"><?= $Total->find(1)['total'] ?> </span>
                 </div>
             </div>
             <!---->
@@ -59,7 +72,8 @@
 				include "front/main.php";
 				}
 				?>
-            <div id="alt" style="position: absolute;width: 350px; min-height: 100px;word-break:break-all;text-align:justify; background-color: rgb(255, 255, 204);top: 50px;left: 400px;z-index: 99;display: none;padding: 5px;border: 3px double rgb(255, 153, 0);background-position: initial initial; background-repeat: initial initial;">
+            <div id="alt"
+                style="position: absolute;width: 350px; min-height: 100px;word-break:break-all;text-align:justify; background-color: rgb(255, 255, 204);top: 50px;left: 400px;z-index: 99;display: none;padding: 5px;border: 3px double rgb(255, 153, 0);background-position: initial initial; background-repeat: initial initial;">
             </div>
             <script>
             $(".sswww").hover(
@@ -79,7 +93,8 @@
             </script>
             <div class="di di ad" style="height:540px;width:23%;padding:0px;margin-left:22px;float:left; ">
                 <!--右邊-->
-                <button style="width:100%;margin-left:auto;margin-right:auto;margin-top:2px;height:50px;" onclick="lo('?do=login')">管理登入</button>
+                <button style="width:100%;margin-left:auto;margin-right:auto;margin-top:2px;height:50px;"
+                    onclick="lo('?do=login')">管理登入</button>
                 <div style="width:89%; height:480px;" class="dbor">
                     <span class="t botli">校園映象區</span>
 
@@ -89,25 +104,29 @@
                         foreach($imgs as $idx => $img):
                     ?>
                     <div class="im cent" id="ssaa<?= $idx ?>">
-                    <img src="upload/<?= $img['img'] ?>" alt="" style="width: 150px;height: 103px; border:3px solid orange;margi:1px">
+                        <img src="upload/<?= $img['img'] ?>" alt=""
+                            style="width: 150px;height: 103px; border:3px solid orange;margi:1px">
                     </div>
                     <?php endforeach; ?>
                     <div class="cent"><img src="./icon/dn.jpg" onclick="pp(2)"></div>
-                                
+
                     <script>
-                    var nowpage=0,num=<?= $Image->count(['sh'=>1]); ?>;
+                    var nowpage = 0,
+                        num = <?= $Image->count(['sh'=>1]); ?>;
 
                     function pp(x) {
                         var s, t;
-                        if (x ==1&&nowpage-1>=0) 
-                            {nowpage--;}
-                        
-                        if (x == 2 && (nowpage*1+3)<num*1) 
-                            {nowpage++;}
-  
+                        if (x == 1 && nowpage - 1 >= 0) {
+                            nowpage--;
+                        }
+
+                        if (x == 2 && (nowpage * 1 + 3) < num * 1) {
+                            nowpage++;
+                        }
+
                         $(".im").hide()
                         for (s = 0; s <= 2; s++) {
-                            t = s * 1+nowpage*1;
+                            t = s * 1 + nowpage * 1;
                             $("#ssaa" + t).show()
                         }
                     }
